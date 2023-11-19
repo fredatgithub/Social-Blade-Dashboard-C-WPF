779022923c23488e834344ba666f8c3e9d02d6b7
fred <domaine75@gmail.com>
2023-11-19 17:25:08 +0100

    clean code and upgrade Nuget packages

--- Dashboard.xaml.cs
+++ Dashboard.xaml.cs
@@ -2,35 +2,26 @@
 using LiveCharts.Defaults;
 using LiveCharts.Wpf;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Social_Blade_Dashboard
 {
-    /// <summary>
-    /// Interaction logic for Dashboard.xaml
-    /// </summary>
-    public partial class Dashboard : UserControl
+  /// <summary>
+  /// Interaction logic for Dashboard.xaml
+  /// </summary>
+  public partial class Dashboard : UserControl
+  {
+    public SeriesCollection SeriesCollection { get; set; }
+    public SeriesCollection LastHourSeries { get; set; }
+    public SeriesCollection LastHourSeries1 { get; set; }
+    public string[] Labels { get; set; }
+    public Func<double, string> Formatter { get; set; }
+    public Dashboard()
     {
-        public SeriesCollection SeriesCollection { get; set; }
-        public SeriesCollection LastHourSeries { get; set; }
-        public SeriesCollection LastHourSeries1 { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double,string> Formatter { get; set; }
-        public Dashboard()
-        {
-            InitializeComponent();
-            SeriesCollection = new SeriesCollection
+      InitializeComponent();
+      SeriesCollection = new SeriesCollection
             {
                 new StackedColumnSeries
                 {
@@ -45,7 +36,7 @@ namespace Social_Blade_Dashboard
                     DataLabels = true
                 }
             };
-            LastHourSeries = new SeriesCollection
+      LastHourSeries = new SeriesCollection
             {
                 new LineSeries
                 {
@@ -65,7 +56,7 @@ namespace Social_Blade_Dashboard
                     }
                 }
             };
-            LastHourSeries1 = new SeriesCollection
+      LastHourSeries1 = new SeriesCollection
             {
                 new LineSeries
                 {
@@ -85,14 +76,14 @@ namespace Social_Blade_Dashboard
                     }
                 }
             };
-            Labels = new[] { "Feb 7", "Feb 8", "Feb 9", "Feb 10" };
-            Formatter = value => value.ToString();
-            DataContext = this;
-            string imgCartoon = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\cartoon-woman-pretty.png";
-            string imgavatar = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\avatar1.jpg";
-            ImgCartoon.Source = new BitmapImage(new Uri(imgCartoon));
-            avatar1.Source = new BitmapImage(new Uri(imgavatar));
-            avatar2.Source = new BitmapImage(new Uri(imgavatar));
-        }
+      Labels = new[] { "Feb 7", "Feb 8", "Feb 9", "Feb 10" };
+      Formatter = value => value.ToString();
+      DataContext = this;
+      string imgCartoon = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent}\\Images\\cartoon-woman-pretty.png";
+      string imgavatar = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent}\\Images\\avatar1.jpg";
+      ImgCartoon.Source = new BitmapImage(new Uri(imgCartoon));
+      avatar1.Source = new BitmapImage(new Uri(imgavatar));
+      avatar2.Source = new BitmapImage(new Uri(imgavatar));
     }
+  }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,39 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Social_Blade_Dashboard
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            RenderPages.Children.Clear();
-            RenderPages.Children.Add(new Dashboard());
-        }
-
-        private void btnExit_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
+      InitializeComponent();
     }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      RenderPages.Children.Clear();
+      RenderPages.Children.Add(new Dashboard());
+    }
+
+    private void BtnExit_Click(object sender, RoutedEventArgs e)
+    {
+      Application.Current.Shutdown();
+    }
+  }
 }
